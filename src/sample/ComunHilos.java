4d332a9e8f32f976f5a4e52453178310b6731035
Yagodiaz98 <yagotudios@gmail.com>
2021-02-15 16:41:11 +0100

    Base del proyecto

---
+++ src/sample/ComunHilos.java
@@ -0,0 +1,9 @@
+package sample;
+
+import java.net.Socket;
+import java.util.ArrayList;
+import java.util.Random;
+
+public class ComunHilos {
+
+}
--- src/sample/Controller.java
+++ src/sample/Controller.java
@@ -1,14 +1,32 @@
 package sample;
 
+import javafx.fxml.FXML;
 import javafx.fxml.Initializable;
+import javafx.scene.control.Button;
+import javafx.scene.control.TextArea;
 
+import java.awt.*;
+import java.io.IOException;
+import java.net.ServerSocket;
+import java.net.Socket;
 import java.net.URL;
 import java.util.ResourceBundle;
 
 public class Controller implements Initializable {
 
+    @FXML
+    Button btnActivarServidor;
+    @FXML
+    Button btnDesactivarServidor;
+    @FXML
+    TextArea txtServidor;
+
+
+    ServerSocket servidor;
+
     @Override
     public void initialize(URL location, ResourceBundle resources) {
-
+        
     }
+
 }
